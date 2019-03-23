46cb112a2026f6485cb7110e2b2016633351a21f
Marco Koch <marco@5th.ch>
2019-03-23 01:08:42 +0000

    rudimentary presence display

--- parseRawNmap.py
+++ parseRawNmap.py
@@ -8,13 +8,15 @@ except KeyError:
     print "first you need to define envorniment variable secret salt (export ", environmentKey, "=blahhh)"
     exit()
 
+print "date,node"
+
 with open(sys.argv[1]) as f:  
    line = f.readline()
    while line:
        line = f.readline()
        if(line.startswith('Starting Nmap')):
-           dateMatch = re.search('\d{4}-\d{2}-\d{2} \d{2}:\d{2} GMT', line)
+           dateMatch = re.search('\d{4}-\d{2}-\d{2} \d{2}:\d{2}', line)
        macMatch = re.search('([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})', line)
        if(macMatch):
            anonymizedMac = hashlib.md5(macMatch.group() + os.environ[environmentKey]).hexdigest()
-           print '{}; {}'.format(dateMatch.group(), anonymizedMac)
+           print '{},{}'.format(dateMatch.group(), anonymizedMac)
